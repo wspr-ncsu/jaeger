15eabdeae8c8cb5b7f0a5e4e3474d278cd649ac6
dahmed <dahmed@ncsu.edu>
2023-10-10 20:00:23 +0000

    added masking and unmasking

--- network-provider/models/oprf.py
+++ network-provider/models/oprf.py
@@ -3,6 +3,13 @@ from oblivious.ristretto import point, scalar
 def msg_to_x(msg):
     return point.hash(msg.encode())
 
+def mask_x(x):
+    c = scalar.random()
+    return (c, c * x)
+
+def unmask_fx(c, fx):
+    return fx / c
+
 def export_x(x):
     return x.to_base64()
 
