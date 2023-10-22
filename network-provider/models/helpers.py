e3641e54c46eb1b9a54e69b5606b01812fee99ac
dahmed <dahmed@ncsu.edu>
2023-10-22 15:17:28 +0000

    replaced namedtupple with actual CDR class

--- network-provider/models/helpers.py
+++ network-provider/models/helpers.py
@@ -1,8 +1,6 @@
 from os import getenv
 from collections import namedtuple
 
-CDR = namedtuple('CDR', ['src', 'dst', 'ts', 'prev', 'curr', 'next'])
-
 def env(envname, default=""):
     value = getenv(envname)
     return value or default
@@ -12,4 +10,21 @@ def not_found():
         
 def toEpoch(date):
     return int(date.timestamp())
+
+class CDR:
+    label = None
+    
+    def __init__(self, src, dst, ts, prev, curr, next):
+        self.src = src
+        self.dst = dst
+        self.ts = toEpoch(ts)
+        self.prev = prev
+        self.curr = curr
+        self.next = next
+        
+    def get_call_detail(self):
+        return f'{self.src}|{self.dst}|{self.ts}'
+    
+    def get_hops(self):
+        return f'{self.prev}|{self.curr}|{self.next}'
 
