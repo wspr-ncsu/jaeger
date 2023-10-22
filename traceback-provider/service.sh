4021c78c4d9ee7ab4bd72da5ed115235b0b63e7b
dahmed <dahmed@ncsu.edu>
2023-10-22 18:55:17 +0000

    Added commands for queue worker

--- traceback-provider/service.sh
+++ traceback-provider/service.sh
@@ -1,9 +1,32 @@
+queue_work="queue:work"
+queue_stop="queue:stop"
+queue_restart="queue:restart"
+
 serve="serve"
 migrate="migrate"
 deps="requirements.txt"
 
 cmd=$1
-allowed_commands=($serve $migrate)
+allowed_commands=($serve $migrate $queue_work $queue_stop $queue_restart)
+
+function queue:work {
+    # if rq.pid.log exists, then a queue worker is already running
+    if [ -f ./logs/rq.pid.log ]
+        then
+        echo "Queue worker is already running. Logs are in ./logs/rq.wrk.log"
+        exit 1
+    fi
+    echo "Starting queue worker for traceback provider"
+    touch ./logs/rq.pid.log
+    rq worker --with-scheduler --pid ./logs/rq.pid.log > ./logs/rq.wrk.log 2>&1 &
+    echo "Queue worker started. Logs are in ./logs/rq.wrk.log"
+}
+
+function queue:stop {
+    echo "Stopping queue worker for traceback provider"
+    kill $(cat ./logs/rq.pid.log)
+    rm ./logs/rq.pid.log
+}
 
 if [[ ! " ${allowed_commands[@]} " =~ " ${cmd} " ]]; then
     echo "Invalid command: $cmd. Valid commands are: ${allowed_commands[@]}"
@@ -40,4 +63,14 @@ elif [ $cmd == $serve ]
     fi
 
     flask run --port $port
+elif [ $cmd == $queue_work ]
+    then
+    queue:work
+elif [ $cmd == $queue_stop ]
+    then
+    queue:stop
+elif [ $cmd == $queue_restart ]
+    then
+    queue:stop 
+    queue:work
 fi
