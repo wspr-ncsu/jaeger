849b5817520506cd46e8eb40e4e6f211070a353f
David L. Adei <kofidahmed@gmail.com>
2024-07-14 19:17:26 +0000

    refactored install script for witenc

--- install-witenc.sh
+++ install-witenc.sh
@@ -1,5 +1,4 @@
 WORKDIR=$(pwd)
 git clone https://github.com/kofi-dalvik/bls-witness-encryption.git
-cd bls-witness-encryption
-python setup.py install
+cd bls-witness-encryption && python setup.py install
 cd $WORKDIR
