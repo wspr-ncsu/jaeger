f9f62425cf3da0d82e13b1c7441629f8516fe1cd
David Love Adei <dahmed@ncsu.edu>
2024-08-30 12:10:20 -0400

    Update install-witenc.sh
    Changed path to library

--- install-witenc.sh
+++ install-witenc.sh
@@ -1,5 +1,5 @@
 WORKDIR=$(pwd)
-git clone https://github.com/kofi-dalvik/bls-witness-encryption.git
+git clone https://github.com/wspr-ncsu/BLS-Witness-Encryption.git bls-witness-encryption
 cd bls-witness-encryption && python setup.py install
 cd $WORKDIR
 rm -rf bls-witness-encryption
